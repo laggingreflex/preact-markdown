3aef4505474cea3f7e2b588ae12f032a3f08d028
laggingreflex <laggingreflex@gmail.com>
2017-03-03 21:53:31 +0530

    fix: trim: false

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ module.exports = Markdown
 
 function Markdown(props) {
   if (typeof props === 'string') {
-    return h(Markup, { markup: toHTML(props) })
+    return h(Markup, {
+      markup: toHTML(props),
+      trim: false,
+    })
   } else if (props && props.markdown) {
     return h(Markup, Object.assign({
-      markup: toHTML(props.markdown)
+      markup: toHTML(props.markdown),
+      trim: false,
     }, props))
   } else {
     throw new Error('Invalid arguments. Markdown requires either a `<String>` or object: `{markdown: <String>}`')
