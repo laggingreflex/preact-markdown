415a776a8e1500b6d597f6a375eee191122e7538
laggingreflex <laggingreflex@gmail.com>
2017-03-30 23:36:47 +0530

    feat: markupOpts & markedOpts

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ var marked = require('marked')
 
 module.exports = Markdown
 
-function Markdown(props) {
+function Markdown(props, opts) {
+  opts = opts || {}
   if (typeof props === 'string') {
-    return h(Markup, {
-      markup: marked(props),
-      trim: false,
-      type: 'html',
-    })
-  } else if (props && props.markdown) {
+    var markupOpts = opts.markupOpts || {}
+    var markdownOpts = opts.markdownOpts || {}
     return h(Markup, Object.assign({
-      markup: marked(props.markdown),
+      markup: marked(props, markdownOpts),
       trim: false,
       type: 'html',
-    }, props))
+    }, opts.markupOpts))
+  } else if (props && props.markdown) {
+    var markupOpts = props.markupOpts || opts.markupOpts || {}
+    var markdownOpts = props.markdownOpts || opts.markdownOpts || {}
+    return h(Markup, Object.assign({
+      markup: marked(props.markdown, markdownOpts),
+      trim: false,
+      type: 'html',
+    }, markupOpts))
   } else {
     throw new Error('Invalid arguments. Markdown requires either a `<String>` or object: `{markdown: <String>}`')
   }
