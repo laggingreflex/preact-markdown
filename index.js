981faf1c574d927b1106c3f53cffcdd0c58d4bc7
laggingreflex <laggingreflex@gmail.com>
2018-11-24 04:32:42 +0530

    refactor

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-var preact = require('preact')
-var Markup = require('preact-markup')
-var marked = require('marked')
+var preact = require('preact');
+var Markup = require('preact-markup');
+var marked = require('marked');
 
-module.exports = Markdown
+module.exports = Markdown;
 
 function Markdown(props, opts) {
-  opts = opts || {}
+  opts = opts || {};
   var h = opts.h || preact.h;
+  var markdown, markupOpts, markdownOpts;
   if (typeof props === 'string') {
-    var markupOpts = opts.markupOpts || {}
-    var markdownOpts = opts.markdownOpts || {}
-    return h(Markup, Object.assign({
-      markup: marked(props, markdownOpts),
-      trim: false,
-      type: 'html',
-    }, opts.markupOpts))
+    markdown = props;
+    props = {};
   } else if (props && typeof props.markdown === 'string') {
-    var markupOpts = props.markupOpts || opts.markupOpts || {}
-    var markdownOpts = props.markdownOpts || opts.markdownOpts || {}
-    return h(Markup, Object.assign({
-      markup: marked(props.markdown, markdownOpts),
-      trim: false,
-      type: 'html',
-    }, markupOpts))
+    markdown = props.markdown;
   } else {
-    throw new Error('Invalid arguments. Markdown requires either a `<String>` or object: `{markdown: <String>}`')
+    throw new Error('Invalid arguments. Markdown requires either a `<String>` or object: `{markdown: <String>}`');
   }
+
+  var markupOpts = props.markupOpts || opts.markupOpts || {};
+  var markdownOpts = props.markdownOpts || opts.markdownOpts || {};
+  return h(Markup, Object.assign({
+    markup: marked(markdown, markdownOpts),
+    trim: false,
+    type: 'html',
+  }, markupOpts));
 }
